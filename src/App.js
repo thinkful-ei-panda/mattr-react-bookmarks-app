0a35ad046686a347fe286019175cf4ee5884617c
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-03 14:21:56 -0700

    Bookmarks App Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import AddBookmark from './addBookmark/addBookmark';
+import BookmarkApp from './bookmarkApp/bookmarkApp';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$SqEBgMvTmagEcE4whUdwv.33xXkdlSc0H9wPMBJaYZf8J3L2mzcle",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    });
+  }
+
+  addBookmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    });
+  }
+
+  render() {
+    const page = this.state.showAddForm
+          ? <AddBookmark 
+                 showForm={show => this.setShowAddForm(show)} 
+                 handleAdd={bookmark => this.addBookmark(bookmark)}/>
+          : <BookmarkApp bookmarks={this.state.bookmarks} showForm={show => this.setShowAddForm(show)}/>; 
+
+    return (
+      <div className="App">
+        { page }
+      </div>
+    );
+  }
 }
 
 export default App;
